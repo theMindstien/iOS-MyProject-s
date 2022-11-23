a473f57e9b4a495b7abc6ee99c384fdd6760a1a9
SAURABH MISHRA <118709210+theMindstien@users.noreply.github.com>
2022-11-23 22:25:28 +0530

    Add files via upload

---
+++ TabBarControllerAndIconImage/TabBarControllerAndIconImage/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  TabBarControllerAndIconImage
+//
+//  Created by apple on 14/11/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onActionLogin(_ sender: Any) {
+        
+        self.tabBarController?.selectedIndex = 1
+        
+        
+    }
+    
+
+}
